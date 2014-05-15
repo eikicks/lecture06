1ce522d1b4cb7ff072ab9802e000f13ce4a9348b
eikicks <eikicks@yhaoo.co.jp>
2014-05-15 13:06:06 +0900

    頑張って解読中

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 var INTERVAL = 1000;
 var DEFAULT_MESSAGE = "終了";
-
+//関数default_messageは終了	
 var alarm = {
 		duration: -1,
 		message: ""
@@ -9,6 +9,7 @@ var alarm = {
 var formatCounterAsString = function(){
 		return "あと" + alarm.duration + "秒";
 };
+//formatcounerassting関数はalarm.durationを　return=計算を返す
 
 var updateCounter = function(){
 		alarm.output.textContent = formatCounterAsString();
@@ -27,17 +28,20 @@ var showAlarmMessage = function(){
 
 var update = function(){
 		alarm.duration = alarm.duration - 1;
+		//alarm.durationのを1引く
 		if(isReadyToCountdown()){
 				updateCounter();
 				window.setTimeout(update, INTERVAL);
+				//update関数をINTERVAL(1秒)ごとに呼び出す
 		}else{
 				showAlarmMessage();
-		}
+		}//falseならshowalrmmessageの関数を呼び出す
 };
 
 var isReadyToCountdown = function(){
 		return Number.isInteger(alarm.duration) && alarm.duration > 0;
 };
+//isReatyToCountdownの関数　実行されalarm.durationの数値が０以上の場合は
 
 var setupAlarm = function(durationString, message){
 		alarm.duration = Number(durationString),
@@ -51,12 +55,16 @@ var startAlarm = function(){
 				window.setTimeout(update, INTERVAL);
 		}
 };
+//isReadyToCountdown関数を呼び出す、trueならupdateCounter関数を呼び出す、trueならupdate関数をINTERVAL(1秒)ごとに呼び出す
+//
 
 var initApp = function(){
 		alarm.durationSelect = document.querySelector("#duration");
+		//タイマーの設定時間(id="duration")
 		alarm.messageInput = document.querySelector("#message");
+		//入力したテキスト(id="message")
 		alarm.output = document.querySelector("#countdown");
-
+		//タイマーのカウントダウン(id="countdown")
 		Notification.requestPermission(function(status){
 				if(Notification.permission != status){
 						Notification.permission = status;
@@ -65,6 +73,7 @@ var initApp = function(){
 
 		var startButton = document.querySelector("#start");
 		startButton.addEventListener("click", startAlarm);
+		//表示する（id="message"）をクリックするとstartAlarmの関数を呼び出す	
 };
-
+//関数initapp　設定する
 initApp();
