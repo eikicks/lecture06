7237118e2a355b3c777030feac53b9b87987ff0c
6cker845 <nick_fun6king@yahoo.co.jp>
2014-05-21 16:38:10 +0900

    プログラムに解説コメントを記入

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 var INTERVAL = 1000;
+//1000mm秒＝1秒
 var DEFAULT_MESSAGE = "終了";
 var alarm = {
 		duration: -1,
@@ -9,13 +10,17 @@ var alarm = {
 var formatCounterAsString = function(){
 		return "あと" + alarm.duration + "秒";
 };
+//プレゼン時間があと何（alarm.durationの値）秒か表示する
 
 var formatCounterAsStringQA = function(){
 		return "あと" + alarm.qtime + "秒";
 };
+//質疑応答時間があと何（alarm.qtimeの値）秒か表示する
+
 
 var updateCounter = function(){
 		alarm.output.textContent = formatCounterAsString();
+		//formatcounerassting関数を呼び出す
 		
 		var QAmessage = "プレゼン時間は残り" + alarm.duration + "秒です";
 		var remainingSec = Number(alarm.duration);
@@ -24,11 +29,12 @@ var updateCounter = function(){
 		if(remainingSec == ringAlarm){
 		   var notification = new Notification(QAmessage);
 		}
+		//プレゼンの残り時間と予鈴の時間が一致した時に、通知（予鈴）をだす。
 };
 
 var updateCounterQA = function(){
 		alarm.output.textContent = formatCounterAsStringQA();
-		console.log(alarm.qtime);
+		//formatCounterAsStringQA関数を呼び出す
 };
 
 var showAlarmMessage = function(){
@@ -37,8 +43,8 @@ var showAlarmMessage = function(){
 		if(Notification.permission == "granted"){
 			var notification = new Notification(message);
 		}
+		//プレゼン時間が終了したときにお知らせの通知をだす。
 		alarm.output.textContent = message;
-		console.log(message);
 };
 
 var showAlarmMessageQA = function(){
@@ -47,8 +53,8 @@ var showAlarmMessageQA = function(){
 		if(Notification.permission == "granted"){
 			var notification = new Notification(message);
 		}
+		//質疑応答時間が終了したときにお知らせの通知をだす。
 		alarm.output.textContent = message;
-		console.log(message);
 };
 
 var update = function(){
@@ -56,6 +62,7 @@ var update = function(){
 		if(isReadyToCountdown()){
 				updateCounter();
 				window.setTimeout(update, INTERVAL);
+				//updatecounter関数をINTERVAL(1秒)ごとに呼び出し、1ずつalarm.durationの値を減らす
 		}else{
 				showAlarmMessage();
 		}
@@ -74,32 +81,41 @@ var updateQA = function(){
 var isReadyToCountdown = function(){
 		return Number.isInteger(alarm.duration) && alarm.duration > 0;
 };
+//isReatyToCountdownの関数　実行されalarm.durationの数値が０以上の場合は実行される
 
 var isReadyToCountdownQA = function(){
 		return Number.isInteger(alarm.qtime) && alarm.qtime > 0;
 };
+//isReatyToCountdownQAの関数　実行されalarm.qtimeの数値が０以上の場合は実行される
 
 var setupAlarm = function(durationString, message){
 		alarm.duration = Number(durationString),
 		alarm.message = message;
 };
+//setupAlarm関数の準備をしている。
 
 var setupQAAlarm = function(durationString, message){
 		alarm.qtime = Number(durationString),
 		alarm.message = message;
 };
+//setupQAAlarm関数の準備をしている。
 
 var startAlarm = function(){
 		setupAlarm(alarm.durationInput.value, alarm.messageInput.value);
+		//タイマーの設定時間の値、アラームのメッセージのテキストの値を代入する。	
 		if(isReadyToCountdown()){
+		 //isReadyToCountdown関数を呼び出す、trueならupdateCounter関数を呼び出す、trueならupdate関数をINTERVAL(1秒)ごとに呼び出す
 				updateCounter();
 				window.setTimeout(update, INTERVAL);
 		}
+		//1秒ずつupdateの関数を呼び出す
 };
 
 var startQAAlarm = function(){
 		setupQAAlarm(alarm.durationQAInput.value, alarm.messageInput.value);
+		//タイマーの設定時間の値、アラームのメッセージのテキストの値を代入する。	
 		if(isReadyToCountdownQA()){
+		 //isReadyToCountdown関数を呼び出す、trueならupdateCounter関数を呼び出す、trueならupdate関数をINTERVAL(1秒)ごとに呼び出す
 				updateCounterQA();
 				window.setTimeout(updateQA, INTERVAL);
 		}
@@ -107,8 +123,11 @@ var startQAAlarm = function(){
 
 var initApp = function(){
 		alarm.durationInput = document.querySelector("#duration");
+		 //タイマーの設定時間(id="duration")
 		alarm.durationQAInput = document.querySelector("#durationQA");
+		//入力したテキスト(id="message")
 		alarm.messageInput = document.querySelector("#message");
+		//タイマーのカウントダウンメッセージ(id="countdown")
 		alarm.output = document.querySelector("#countdown");
 		Notification.requestPermission(function(status){
 				if(Notification.permission != status){
@@ -118,8 +137,11 @@ var initApp = function(){
 		
 		var startButton = document.querySelector("#start");
 		startButton.addEventListener("click", startAlarm);
+		//startAlarmの関数を呼び出す
 		var startQAButton = document.querySelector("#startQA");
 		startQAButton.addEventListener("click", startQAAlarm);
+		//startQAAlarmの関数を呼び出す
+
 };
 
 initApp();
